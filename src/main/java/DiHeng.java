ff8313a110aac8b59fca8ae3745f46030bc39e54
Hoo Di Heng <Hoodiheng@gmail.com>
2025-08-21 00:58:52 +0800

    Add support for echo

--- src/main/java/DiHeng.java
+++ src/main/java/DiHeng.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
+
 public class DiHeng {
     public static void main(String[] args) {
         String greeting = """
                 Hello from Di Heng!!!
-                Please let me know what you need.
                 It is a pleasure to meet you.
+                Please let me know what you need...
                 """;
         System.out.println(greeting);
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            } else {
+                System.out.println(input);
+            }
+        }
+        System.out.println("Goodbye!");
+
     }
 }
