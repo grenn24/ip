63d79de58a89f3c33940e6d75625e9837a852c57
Hoo Di Heng <Hoodiheng@gmail.com>
2025-08-21 01:04:45 +0800

    Add support for list

--- src/main/java/DiHeng.java
+++ src/main/java/DiHeng.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class DiHeng {
     public static void main(String[] args) {
+        List<String> inputs = new ArrayList<>();
         String greeting = """
                 Hello from Di Heng!!!
                 It is a pleasure to meet you.
@@ -10,11 +13,16 @@ public class DiHeng {
         System.out.println(greeting);
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
-            String input = scanner.nextLine();
-            if (input.equals("bye")) {
+            String curr = scanner.nextLine();
+            if (curr.equals("bye")) {
                 break;
+            } else if (curr.equals("list")) {
+                for (int i = 0; i < inputs.size(); i++) {
+                    System.out.printf("%d: %s\n", i + 1 ,inputs.get(i));
+                }
             } else {
-                System.out.println(input);
+                inputs.add(curr);
+                System.out.printf("Added: %s\n", curr);
             }
         }
         System.out.println("Goodbye!");
