3289b32deb60f8b43f1179628ace59d106eeb0b0
Hoo Di Heng <Hoodiheng@gmail.com>
2025-08-21 00:53:08 +0800

    rename Duke.java

---
+++ src/main/java/DiHeng.java
@@ -0,0 +1,10 @@
+public class DiHeng {
+    public static void main(String[] args) {
+        String greeting = """
+                Hello from Di Heng!!!
+                Please let me know what you need.
+                It is a pleasure to meet you.
+                """;
+        System.out.println(greeting);
+    }
+}
