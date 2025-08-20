46e302e8fd19dbae30b643a0a9a22981c16d7cf9
Hoo Di Heng <Hoodiheng@gmail.com>
2025-08-21 01:10:31 +0800

    Add task.java

--- src/main/java/DiHeng.java
+++ src/main/java/DiHeng.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public class DiHeng {
     public static void main(String[] args) {
-        List<String> inputs = new ArrayList<>();
+        List<Task> inputs = new ArrayList<>();
         String greeting = """
                 Hello from Di Heng!!!
                 It is a pleasure to meet you.
@@ -17,11 +17,14 @@ public class DiHeng {
             if (curr.equals("bye")) {
                 break;
             } else if (curr.equals("list")) {
+                System.out.println("Here are the pending tasks:");
                 for (int i = 0; i < inputs.size(); i++) {
-                    System.out.printf("%d: %s\n", i + 1 ,inputs.get(i));
+                    Task task = inputs.get(i);
+                    System.out.printf("%d[%s]: %s\n", i + 1 , task.isCompleted() ? "X" : " "  , task.getDescription());
                 }
             } else {
-                inputs.add(curr);
+                Task currTask = new Task(curr);
+                inputs.add(currTask);
                 System.out.printf("Added: %s\n", curr);
             }
         }
---
+++ src/main/java/Task.java
@@ -0,0 +1,20 @@
+public class Task {
+    private final String description;
+    private boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
