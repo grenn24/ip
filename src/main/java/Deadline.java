e5b8fe2f0cd49c694a418753683a6bfc1df1e9d7
Hoo Di Heng <Hoodiheng@gmail.com>
2025-08-25 16:43:50 +0800

    Add default date time format

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -5,19 +5,21 @@ import java.time.format.DateTimeFormatter;
 public class Deadline extends Task {
 
     private final LocalDateTime deadline;
+    private final DateTimeFormatter DATE_TIME_FORMAT = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
 
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = LocalDateTime.parse(deadline);
+        this.deadline = LocalDateTime.parse(deadline, DATE_TIME_FORMAT
+        );
     }
 
     public Deadline(String description, String deadline, boolean isCompleted) {
         super(description, isCompleted);
-        this.deadline = LocalDateTime.parse(deadline);
+        this.deadline = LocalDateTime.parse(deadline, DATE_TIME_FORMAT);
     }
 
     @Override
     public String toString() {
-        return String.format("[D][%s] %s (by %s)", super.isCompleted() ? "X" : " ", super.getDescription(), deadline.format(DateTimeFormatter.ofPattern("MMM dd, yyyy HH:mm")));
+        return String.format("[D][%s] %s (by: %s)", super.isCompleted() ? "X" : " ", super.getDescription(), this.deadline.format(DATE_TIME_FORMAT));
     }
 }
