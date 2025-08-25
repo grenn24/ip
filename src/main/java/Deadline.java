bd08a850e2bb697a45e8c470bffc6f69fdfc95c3
Hoo Di Heng <Hoodiheng@gmail.com>
2025-08-25 16:21:05 +0800

    Add support for storing deadline as Dates

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,18 @@
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private final String deadline;
+
+    private final LocalDate deadline;
+
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+        this.deadline = LocalDate.parse(deadline);
     }
 
     @Override
     public String toString() {
-        return String.format("[D][%s] %s (by: %s)", super.isCompleted() ? "X" : " ", super.getDescription(), deadline);
+        return String.format("[D][%s] %s (by %s)", super.isCompleted() ? "X" : " ", super.getDescription(), deadline.format(DateTimeFormatter.ofPattern("MMM dd yyyy")));
     }
 }
